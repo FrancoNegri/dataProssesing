7eaf089eb814557c990b67f3441a76d6feeaa81a
Franco Negri <franconegri2004@gmail.com>
2018-01-23 21:58:13 -0300

    Added nationality data

--- orderlul.py
+++ orderlul.py
@@ -1,5 +1,6 @@
 import os, errno
 f = open("datos.txt")
+g = open("alldata.txt", "w")
 for line in f:
     line = line.split('\t')
     if not os.path.exists(line[0]):
@@ -7,6 +8,7 @@ for line in f:
     if not os.path.exists(line[0] +  "/" +line[1]):
         os.makedirs(line[0] +  "/" +line[1])
     y = open(line[0] +  "/" +line[1] + "/" + "data.txt", "a")
-    y.write(line[6])
+    y.write(line[5] + "\n")
+    g.write(line[5] + "\n")
 
 
